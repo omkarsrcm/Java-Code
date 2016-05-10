56995e1199e50e6a931818adc88539419cb34992
root <root@ip-172-31-24-130.us-west-2.compute.internal>
2016-05-10 19:21:35 +0530

    updated file

--- src/main/java/za/co/scrinium/ecommerce/core/domain/SecureUserDetails.java
+++ src/main/java/za/co/scrinium/ecommerce/core/domain/SecureUserDetails.java
@@ -21,6 +21,7 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+
 package za.co.scrinium.ecommerce.core.domain;
 
 import org.springframework.security.core.GrantedAuthority;
